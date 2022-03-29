820172d21246251ba96c736c19209c3c4a6169d7
ahsanz2 <ahsan.zafar@shopdev.co>
2022-03-29 15:33:17 +0500

    feat: sample language translation text added

--- components/lazurde/hero-banner/index.tsx
+++ components/lazurde/hero-banner/index.tsx
@@ -1,5 +1,6 @@
 import { AppContext } from "lib/context";
 import { ImageObject } from "lib/types/common";
+import useTranslation from "next-translate/useTranslation";
 import Image from "next/image";
 import React, { FC, useContext, useEffect } from "react";
 import styles from "./Hero-banner.module.css";
@@ -18,6 +19,7 @@ const LazurdeHeroBanner: FC<LazurdeHeroBannerProps> = ({
   buttonLink,
 }): JSX.Element => {
   const { brand, setBrand } = useContext(AppContext);
+  const { t } = useTranslation("common");
   useEffect(() => {
     setBrand("missl");
   }, []);
@@ -34,6 +36,7 @@ const LazurdeHeroBanner: FC<LazurdeHeroBannerProps> = ({
       />
       <div className={styles["banner-text-section"]}>
         <h3>{bannerText || ""}</h3>
+        <h5>{t("sampleTranslationText")}</h5>
         <button>{buttonText || ""}</button>
       </div>
     </div>
