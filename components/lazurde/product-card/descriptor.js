0c29bea21894160263a9d677381e285b5a04be3f
wasif <swash99@gmail.com>
2022-03-30 18:26:28 +0500

    chore: trigger new build for descriptor fix

--- components/lazurde/product-card/descriptor.js
+++ components/lazurde/product-card/descriptor.js
@@ -2,7 +2,7 @@ const { Types } = require("@teamfabric/xpm");
 
 exports.default = Types.Component({
   id: "ProductCard",
-  label: "Product Card",
+  label: "Product Cards",
   description: ``,
 
   attributes: {
