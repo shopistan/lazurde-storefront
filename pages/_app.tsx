92485524bef1365ceea9f7b9c6d2e186fef5e737
wasif <swash99@gmail.com>
2022-04-08 13:20:00 +0500

    fix: changed next app type props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ContextProvider from "lib/context";
 
-function LazurdeApp({ Component, pageProps }: AppProps) {
+function LazurdeApp({ Component, pageProps }: any): JSX.Element {
   return (
     <ContextProvider>
       <Component {...pageProps} />
