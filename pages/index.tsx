f69a8d513537b7c7f06dedfbffe6146914c3b0ba
Mirza Safeer  Baig <safeer.baig@shopdev.co>
2022-04-08 12:24:33 +0000

    Revert "Merge branch 'LW-167' into 'develop'"
    This reverts merge request !48

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,36 @@ import { fetchGlobalComponents, fetchXMComponents } from "lib/xm";
 import React, { FC } from "react";
 import AppContentWrapper from "../components/common/app-content-wrapper";
 import styles from "../styles/Home.module.css";
+import ExploreBrands from "components/lazurde/explore-brands";
 const LazurdeHome: FC<PageProps> = ({
   headerProps,
   footerProps,
   pageComponents,
 }) => {
+
+  const exploreData = [
+    {
+      title : 'Lazurd`e',
+      backgroundImage : {
+        url : '',
+        altText : 'Image1',
+      }
+    },
+    {
+      title : 'Missl',
+      backgroundImage : {
+        url : '',
+        altText : 'Image2',
+      }
+    },
+    {
+      title : 'Kenaz',
+      backgroundImage : {
+        url : '',
+        altText : 'Image3',
+      }
+    },
+  ]
   return (
     <>
       <Header {...headerProps}></Header>
@@ -51,6 +76,7 @@ const LazurdeHome: FC<PageProps> = ({
             return null;
           })}
         </div>
+        <ExploreBrands exploreBrandsArray={exploreData} />
       </AppContentWrapper>
       <Footer {...footerProps}></Footer>
     </>
