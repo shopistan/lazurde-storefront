c8fdd55f1296e0a5cf2f413567c6c3b467a33238
wasif <swash99@gmail.com>
2022-04-08 16:32:38 +0500

    chore: added brandsidebar in xm component list

--- components/xm-component-library.ts
+++ components/xm-component-library.ts
@@ -3,5 +3,6 @@ import ProductCard from "./lazurde/product-card";
 import Header from "./common/header";
 import Footer from "./common/footer";
 import BambuserPopup from './common/bambuser-popup/index';
+import BrandSideBar from './common/header/user-navbar/brand-sidebar/index';
 
-export const componentsById: any = { LazurdeHeroBanner, BambuserPopup, ProductCard , Header, Footer };
+export const componentsById: any = { LazurdeHeroBanner, BambuserPopup, ProductCard , Header, BrandSideBar, Footer };
