e263a96ce1ce1605502bb18cb10e1bcc7effa3eb
wasif <swash99@gmail.com>
2022-04-12 15:58:42 +0500

    chore: header descriptor test

--- components/common/header/descriptor.js
+++ components/common/header/descriptor.js
@@ -1,15 +1,40 @@
 const { Types } = require("@teamfabric/xpm");
 
 exports.default = Types.Component({
-    id: "Header",
-    label: "Header",
-    description: ``,
-    isGlobal: true,
+  id: "Header",
+  label: "Header",
+  description: ``,
+  isGlobal: true,
 
-    attributes: {
-        promoTitle: Types.String({ label: 'Promo bar title' }),
-        promoLinkText: Types.String({ label: 'Promo bar link text' }),
-        mobilePromoLinkText: Types.String({ label: 'Mobile Promo bar link text' }),
-        promoLink: Types.String({ label: 'Promo bar link' })
-    },
+  attributes: {
+    promoTitle: Types.String({ label: "Promo bar title" }),
+    promoLinkText: Types.String({ label: "Promo bar link text" }),
+    mobilePromoLinkText: Types.String({ label: "Mobile Promo bar link text" }),
+    promoLink: Types.String({ label: "Promo bar link" }),
+
+    children: Types.Shape({
+      label: "Site Navbar",
+      children: {
+        title: Types.String({ label: "Promo bar link" }),
+      },
+    }),
+
+    // footerLinks: Types.Array({
+    //     label: "Footer Links",
+    //     children: Types.Shape({
+    //       children: {
+    //         linkHeading: Types.String({ label: "Links heading" }),
+    //         links: Types.Array({
+    //           label: "Links",
+    //           children: Types.Shape({
+    //             children: {
+    //               text: Types.String({ label: "Title" }),
+    //               url: Types.String({ label: "URL" }),
+    //             },
+    //           }),
+    //         }),
+    //       },
+    //     }),
+    //   }),
+  },
 });
