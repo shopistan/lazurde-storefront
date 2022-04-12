c347874d11bbe6d6351e2e5413af988b8da69a87
wasif <swash99@gmail.com>
2022-04-12 10:39:35 +0500

    chore: select dropdown added arrow and image

--- components/common/ui/select/index.tsx
+++ components/common/ui/select/index.tsx
@@ -1,8 +1,9 @@
+import ArrowDown from "components/icons/ArrowDown";
 import React, { useState, useRef, useEffect } from "react";
 
 import styles from "./style.module.scss";
 
-type optionProps = { label: string, value: string }
+type optionProps = { label: string, img: string, value: string }
 
 interface SelectProps {
   options: optionProps[],
@@ -10,16 +11,16 @@ interface SelectProps {
   defaultValue: string,
 }
 
-const Select = ({ options = [{ label: 'label', value: 'value' }], onChange, defaultValue }: SelectProps): JSX.Element => {
+const Select = ({ options = [{ label: 'label', img: '', value: 'value' }], onChange, defaultValue }: SelectProps): JSX.Element => {
   const dropdown = useRef(null)
-  const [selectedVal, setSelectedVal] = useState<optionProps>({ label: defaultValue, value: defaultValue })
+  const [selectedVal, setSelectedVal] = useState<optionProps>({ label: defaultValue, img: defaultValue, value: defaultValue })
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [position, setPosition] = useState<string>('bottom')
 
-  useEffect (() => {
-    setSelectedVal({ label: defaultValue, value: defaultValue })
+  useEffect(() => {
+    setSelectedVal({ label: defaultValue, img: defaultValue, value: defaultValue })
   }, [defaultValue])
-  
+
   return (
     <div ref={dropdown} tabIndex={0} className={styles["dropdown"]} data-open={isOpen} onBlur={() => setIsOpen(false)}  >
       <span className={styles["select"]} onClick={() => {
@@ -31,6 +32,7 @@ const Select = ({ options = [{ label: 'label', value: 'value' }], onChange, defa
         setIsOpen(!isOpen)
       }}>
         {selectedVal.label}
+        {selectedVal.img && <img src={selectedVal.img} width='16px' alt="image" />}
       </span>
       <ul className={`${styles["options-ul"]}`} data-position={position}>
         {options.map((opData, index) => {
@@ -43,10 +45,16 @@ const Select = ({ options = [{ label: 'label', value: 'value' }], onChange, defa
                 onChange(opData)
 
               }}>
-              <a>{opData.label}</a>
+              <a>{opData.label}
+
+                {opData.img && <img src={opData.img} width='16px' alt="image" />}
+              </a>
             </li>)
         })}
       </ul>
+      <div className={styles['select-arrow']}>
+        <ArrowDown />
+      </div>
     </div>
   );
 };
