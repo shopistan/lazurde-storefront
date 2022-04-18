93b807fcb38a2adc82f0ca959f94a06880e4036d
safeer <safeer.baig@shopdev.co>
2022-04-18 11:07:11 +0500

    fix: styling of cards

--- components/common/brands-card/index.tsx
+++ components/common/brands-card/index.tsx
@@ -1,11 +1,11 @@
-import React, { FC , useContext } from "react";
+import React, { FC, useContext } from "react";
 import Cards from "components/common/card";
 import { ImageType } from "lib/types/common";
 import styles from "./brands-card.module.scss";
 import useWindowSize from "lib/utils/useWindowSize";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {AppContext} from 'lib/context/index'
+import { AppContext } from 'lib/context/index'
 import useTranslation from "next-translate/useTranslation";
 interface ExploreBrandsProps {
   cardTitle: string;
@@ -24,8 +24,8 @@ const BrandsCard: FC<ExploreBrandArray> = ({
   const [width] = useWindowSize();
   const { appState } = useContext(AppContext);
   const { t } = useTranslation("common");
-  const onClick = () => {};
-  
+  const onClick = () => { };
+
   return (
     <div className={styles["cards-container"]}>
       <h3 data-testid='heading' className={styles["cards-heading"]}>{appState?.lang === "en" ? heading : t("cardHeading")}</h3>
@@ -39,27 +39,27 @@ const BrandsCard: FC<ExploreBrandArray> = ({
         key={appState?.lang}
         dir={appState?.lang === "en" ? "ltr" : "rtl"}
       >
-      <div className={`flex justify-between`}>
-        {exploreBrandsArray &&
-          exploreBrandsArray.map((data, index) => {
-            const { cardTitle, cardImage } = data;
-            return (
-              <SwiperSlide key={index}>
+        <div className={`flex justify-between`}>
+          {exploreBrandsArray &&
+            exploreBrandsArray.map((data, index) => {
+              const { cardTitle, cardImage } = data;
+              return (
+                <SwiperSlide key={index}>
 
-              <div className={styles["cards"]} key={index}>
-                <Cards
-                  onClick={onClick}
-                  className={styles["brand-card"]}
-                  height="100%"
-                  width="100%"
-                  cardTitle={cardTitle}
-                  cardImage={cardImage}
-                />
-              </div>
-              </SwiperSlide>
-            );
-          })}
-      </div>
+                  <div className={`${styles["cards"]}  ${appState.lang == 'ar' && styles["arabic-title"]}`} key={index}>
+                    <Cards
+                      onClick={onClick}
+                      className={`${styles["brand-card"]}`}
+                      height="100%"
+                      width="100%"
+                      cardTitle={cardTitle}
+                      cardImage={cardImage}
+                    />
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+        </div>
       </Swiper>
     </div>
   );
