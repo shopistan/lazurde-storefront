b0e39d817d5ecf57d9b9f80eebdf46679c677492
UmairKhan <umair.baloch@shopdev.co>
2022-04-18 10:50:04 +0500

    fix: mobile header & brand cards importing issue fixed

--- components/xm-component-library.ts
+++ components/xm-component-library.ts
@@ -3,7 +3,7 @@ import Header from "./common/header";
 import Footer from "./common/footer";
 import BambuserPopup from "./common/bambuser-popup/index";
 import BrandSideBar from "./common/header/user-navbar/brand-sidebar/index";
-import BrandsCard from "./common/brands-card/index";
+import BrandsCard from "./common/brand-cards/index";
 import CardSlider from "./common/card-slider";
 import CollectionCard from "./common/collection-card";
 
