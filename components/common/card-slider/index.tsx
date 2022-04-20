e1ae33237f7faca8226d4deb1b6e0e07c0f52c1f
UmairKhan <umair.baloch@shopdev.co>
2022-04-20 11:35:50 +0500

    fix: category slider arabic content fixes

--- components/common/card-slider/index.tsx
+++ components/common/card-slider/index.tsx
@@ -16,12 +16,13 @@ import Label from "../ui/label";
 type CardsArrType = {
   image?: { url: string; altText: string };
   heading?: string;
-}
+};
 interface CardSliderProps {
   className?: string;
   sectionHeading?: string;
   bgColor?: string;
   cards?: CardsArrType[];
+  testId?: string;
 }
 
 const CardSlider = ({
@@ -29,22 +30,24 @@ const CardSlider = ({
   className = "",
   sectionHeading = "",
   bgColor = "#fff",
+  testId = "",
 }: CardSliderProps): JSX.Element => {
   const [width] = useWindowSize();
   const { appState } = useContext(AppContext);
   const { t } = useTranslation("common");
 
-  const _cards =
-    appState?.lang === "en"
-      ? cards
-      : t("cardSliderData.cards", {}, { returnObjects: true });
+  const _arabicCardData = t(
+    "cardSliderData.cards",
+    {},
+    { returnObjects: true }
+  );
 
   return (
     <div
       className={`${styles["card-slider__wrapper"]} ${className}`}
       style={{ backgroundColor: bgColor }}
     >
-      <Label className={styles["card-slider__section-heading"]}>
+      <Label testId={testId} className={styles["card-slider__section-heading"]}>
         {appState?.lang === "en"
           ? sectionHeading
           : t("cardSliderData.sectionHeading")}
@@ -60,15 +63,21 @@ const CardSlider = ({
         dir={appState?.lang === "en" ? "ltr" : "rtl"}
       >
         <>
-          {Array.isArray(_cards) &&
-            _cards.length > 0 &&
-            _cards.map((content, i) => {
+          {Array.isArray(cards) &&
+            cards.length > 0 &&
+            cards.map((content, index) => {
               const { image, heading } = content;
               return (
-                <SwiperSlide key={i}>
+                <SwiperSlide key={index}>
                   <Cards
                     cardImage={image}
-                    cardTitle={heading}
+                    cardTitle={
+                      appState?.lang === "en"
+                        ? heading
+                        : Array.isArray(_arabicCardData) &&
+                          _arabicCardData.length > 0 &&
+                          _arabicCardData[index].heading
+                    }
                     width={width > 1023 ? 314 : 332}
                     height={width > 1023 ? 429 : 352}
                     className="category-slider-card"
