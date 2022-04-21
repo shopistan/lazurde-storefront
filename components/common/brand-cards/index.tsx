8d1deefd7f2e147ea635449ef827d5d0c911a5d0
safeer <safeer.baig@shopdev.co>
2022-04-21 13:13:58 +0500

    fix: brand card slider

--- components/common/brand-cards/index.tsx
+++ components/common/brand-cards/index.tsx
@@ -7,11 +7,12 @@ import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { AppContext } from "lib/context/index";
 import useTranslation from "next-translate/useTranslation";
+import Slider from "components/common/ui/slider/slider";
 
 type BrandCardsType = {
   cardTitle: string;
   cardImage: ImageType;
-  favIconSrc : ImageType;
+  favIconSrc: ImageType;
 };
 
 interface BrandCardsProps {
@@ -26,27 +27,24 @@ const BrandCards: FC<BrandCardsProps> = ({
   const [width] = useWindowSize();
   const { appState } = useContext(AppContext);
   const { t } = useTranslation("common");
-  const onClick = () => {};
+  const onClick = () => { };
 
   return (
     <div className={styles["cards-container"]}>
       <h3 data-testid="heading" className={styles["cards-heading"]}>
         {appState?.lang === "en" ? heading : t("cardHeading")}
       </h3>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={8}
-        slidesPerView={width > 1023 ? 3 : 1.29}
+      <Slider
+        desktopSlidePerView={3}
+        mobileSlidePerView={1.1}
         navigation={width > 1023 ? true : false}
-        scrollbar={{ draggable: true }}
+        scrollbar={true}
         className={`card-brands`}
-        key={appState?.lang}
-        dir={appState?.lang === "en" ? "ltr" : "rtl"}
       >
         <div className={`flex justify-between`}>
           {brandCards &&
             brandCards.map((data, index) => {
-              const { cardTitle, cardImage , favIconSrc } = data;
+              const { cardTitle, cardImage, favIconSrc } = data;
               return (
                 <SwiperSlide key={index}>
                   <div className={`${styles["cards"]} ${appState.lang == 'ar' && styles["arabic-card"]}`} key={index}>
@@ -64,7 +62,7 @@ const BrandCards: FC<BrandCardsProps> = ({
               );
             })}
         </div>
-      </Swiper>
+      </Slider>
     </div>
   );
 };
