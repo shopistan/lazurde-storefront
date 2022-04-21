0ed22a2d66dd1ee12ffcea0339cd04e95ee6ac75
ahsanz2 <ahsan.zafar@shopdev.co>
2022-04-22 01:41:47 +0500

    feat: initial category search logic added to listing page

--- pages/c/[...category_hierarchy].tsx
+++ pages/c/[...category_hierarchy].tsx
@@ -12,6 +12,7 @@ import AppContentWrapper from "components/common/app-content-wrapper";
 import { componentsById } from "components/xm-component-library";
 import Footer from "components/common/footer";
 import { PageRouteType } from "lib/types/xm";
+import { fetchCategoryProducts } from "lib/algolia";
 
 const LazurdeProductListingPage: FC<PageProps> = ({
   headerProps,
@@ -83,12 +84,36 @@ export const getStaticProps: GetStaticProps = async (context: any) => {
         (item: XMComponent) => item.id === "BrandSideBar"
       ) || {}
     ).params || {};
+
+  const { categoryName = null } = (
+    pageComponents.find(
+      (component: XMComponent) => component.id === "ProductListing"
+    ) || {}
+  ).params;
+
+  let searchResults;
+
+  if (categoryName) {
+    searchResults = await fetchCategoryProducts({
+      categoryName,
+    });
+  }
+
+  const { hits = [], nbHits, page, nbPages, hitsPerPage } = searchResults || {};
+
   return {
     props: {
       headerProps,
       footerProps,
       brandSidebarProps,
       pageComponents,
+      algoliaSearchResults: {
+        hits,
+        nbHits,
+        page,
+        nbPages,
+        hitsPerPage,
+      },
     },
     revalidate: 5,
   };
