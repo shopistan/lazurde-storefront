05c100e380d841956a053b6518b08136a39c6bd5
safeer <safeer.baig@shopdev.co>
2022-04-21 13:04:18 +0500

    fix: explore all category

---
+++ components/common/plp-category/index.tsx
@@ -0,0 +1,38 @@
+import { ImageType } from 'lib/types/common';
+import React, { FC } from 'react'
+import Card from '../card/index'
+import Label from '../ui/label'
+
+type PLPCategoryTypes = {
+    cardTitle: string;
+    cardImage: ImageType;
+}
+
+interface PLPCategoryProps {
+    title: string;
+    text: string;
+    cards: PLPCategoryTypes[];
+}
+
+const PLPCategory: FC<PLPCategoryProps> = ({ title, text, cards }) => {
+    return (
+        <> {
+            title &&
+            <Label>{title}</Label>
+        }
+            {
+                text &&
+                <Label>{text}</Label>
+            }
+            {
+                cards && cards.length > 0 && cards.map((data, index) => {
+                    const { cardImage, cardTitle } = data
+                    return (
+                        <Card cardImage={cardImage} cardTitle={cardTitle} key={index} />
+                    )
+                })
+            }
+        </>
+    )
+}
+export default PLPCategory
