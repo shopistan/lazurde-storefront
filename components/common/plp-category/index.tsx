5decab38af65c021ae9e7af0c637083708c4acbf
safeer <safeer.baig@shopdev.co>
2022-04-22 10:57:41 +0500

    fix: explore all category

--- components/common/plp-category/index.tsx
+++ components/common/plp-category/index.tsx
@@ -9,22 +9,25 @@ import useTranslation from "next-translate/useTranslation";
 import { useRouter } from 'next/router'
 
 type PLPCategoryTypes = {
-    cardTitle: string;
-    cardImage: ImageType;
+    cardTitle?: string;
+    cardImage?: ImageType;
 }
 
 type _PLPCategoryTypes = {
-    cardTitle: string;
+    cardTitle?: string;
 }
 
 
 interface PLPCategoryProps {
-    title: string;
-    text: string;
-    cards: PLPCategoryTypes[];
+    title?: string;
+    isLeft?: Boolean;
+    isRight?: Boolean;
+    isCentre?: Boolean;
+    text?: string;
+    cards?: PLPCategoryTypes[];
 }
 
-const PLPCategory: FC<PLPCategoryProps> = ({ title = '', text = '', cards = [] }) => {
+const PLPCategory: FC<PLPCategoryProps> = ({ title = '', text = '', cards = [], isLeft = false, isRight = false, isCentre = true }) => {
     const { appState } = useContext(AppContext);
     const router = useRouter()
     const { t } = useTranslation("common");
@@ -36,16 +39,16 @@ const PLPCategory: FC<PLPCategoryProps> = ({ title = '', text = '', cards = [] }
     const [width] = useWindowSize();
     return (
         <div className={styles['plpCategory-container']}>
-            <Label className={styles["plpCategory-title"]}>{appState.lang == 'en' ? title : t('plpCategoryTitle')}</Label>
-            <Label className={styles["plpCategory-text"]}>{appState.lang == 'en' ? text : t('plpCategoryText')}</Label>
-            <div className={styles['plpCategory-card']}>
+            <Label className={` ${isLeft && 'text-left'} ${isCentre && 'text-center'} ${isRight && 'text-right'} ${styles["plpCategory-title"]}`}>{appState.lang == 'en' ? title : t('plpCategoryTitle')}</Label>
+            <Label className={`${isLeft && 'text-left'} ${isCentre && 'text-center'} ${isRight && 'text-right'} ${styles["plpCategory-text"]}`}>{appState.lang == 'en' ? text : t('plpCategoryText')}</Label>
+            <div className={` ${isLeft && 'text-left'} ${isCentre && 'text-center'} ${isRight && 'text-right'} ${styles['plpCategory-card']}`}>
 
                 {
                     cards.length > 0 && cards.map((card, index) => {
-                        const { cardImage, cardTitle} = card
+                        const { cardImage, cardTitle } = card
                         return (
-                            <div onClick={() => { router.push('/') }}>
-                                <Cards width={width > 1023 ? 314 : 167.5} height={width > 1024 ? 331 : 180.5} className={`plp-card`} cardImage={cardImage} cardTitle={appState.lang == 'en' ? cardTitle : _plpCategoryCard[index].cardTitle} key={index} />
+                            <div onClick={() => { router?.push('/') }}>
+                                <Cards width={width > 1023 ? 314 : 167.5} height={width > 1024 ? 331 : 180.5} className={`plp-card`} cardImage={cardImage} cardTitle={appState?.lang == 'en' ? cardTitle : _plpCategoryCard[index]?.cardTitle} key={index} />
                             </div>
 
                         )
