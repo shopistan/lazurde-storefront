999b58d1cf19475d0aa2c1e5e5a02dd3644e3476
safeer <safeer.baig@shopdev.co>
2022-04-27 01:26:15 +0500

    fix: regiter pop up

--- components/common/register-popup/descriptor.js
+++ components/common/register-popup/descriptor.js
@@ -7,12 +7,12 @@ exports.default = Types.Component({
 
     attributes: {
         modalImage: Types.Image({
-            label : 'Image',
-            url : Types.String({label : 'URL'}),
-            altText : Types.String({label : 'Alt Text'}),
+            label: 'Image',
+            url: Types.String({ label: 'URL' }),
+            altText: Types.String({ label: 'Alt Text' }),
         }),
-    modalTitle: Types.String({label : 'Title'}),
-    modalText: Types.String({label : 'Text'}),
-    modalButton: Types.String({label : 'Button Text'}),
+        modalTitle: Types.String({ label: 'Title' }),
+        modalText: Types.String({ label: 'Text' }),
+        modalButton: Types.String({ label: 'Button Text' }),
     },
 });
