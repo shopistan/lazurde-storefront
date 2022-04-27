c83bf8b1884ca1d03887f0322e097681454fc92c
safeer <safeer.baig@shopdev.co>
2022-04-27 12:23:10 +0500

    fix: regiter pop up

--- components/common/register-popup/index.tsx
+++ components/common/register-popup/index.tsx
@@ -1,30 +1,34 @@
-import React, { FC } from 'react'
+import React, { useState, FC } from 'react'
 import Image from 'next/image'
 import Label from 'components/common/ui/label/index'
 import Button from 'components/common/ui/button/index'
 import Modal from 'components/common/ui/modal/index'
 import styles from './register-popup.module.scss'
 import { ImageType } from 'lib/types/common'
+import CrossSmall from 'components/icons/CrossSmall'
 
 interface RegisterModalProps {
     modalImage: ImageType;
     modalTitle: string;
     modalText: string;
     modalButton: string;
+    isOpen: boolean;
 }
 
 
-const RegisterModal: FC<RegisterModalProps> = ({ modalImage, modalTitle = '', modalText = '', modalButton = '' }) => {
+const RegisterModal: FC<RegisterModalProps> = ({ isOpen = false, modalImage, modalTitle = '', modalText = '', modalButton = '' }) => {
+    const [open, setOpen] = useState(isOpen)
     return (
-        <Modal className='register-modal' isOpened={true} onClose={() => { }}  >
+        <Modal className='register-modal' isOpened={open} onClose={() => { }}  >
             <div className={styles['register-container']}>
+                <div className={styles['cross-icon']} onClick={() => { setOpen(!open) }}>
+                    <CrossSmall />
+                </div>
                 {
                     modalImage && modalImage.url &&
-                    <Image className={styles['modal-image']} src={modalImage.url} alt={modalImage.altText} width={500} height={400} />
+                    <Image className={styles['modal-image']} src={modalImage.url} alt={modalImage.altText} width={300} height={400} />
                 }
-                <div >
-                </div>
-                <div>
+                <div className={styles['register-text-section']}>
                     <Label className={styles['modal-title']}>
                         {modalTitle}
                     </Label>
