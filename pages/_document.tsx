2b00a838f9b4e738c31a6af970332e6a034a3ee3
ahsanz1 <ahsan.zafar@shopdev.co>
2022-05-14 01:32:34 +0500

    feat: font display block

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,7 @@ class LazurdeDocument extends Document {
             crossOrigin="anonymous"
           ></link>
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=fallback"
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=optional"
             rel="stylesheet"
           ></link>
           <link
