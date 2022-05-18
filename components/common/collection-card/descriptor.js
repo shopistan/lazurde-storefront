626c8437b2fac92f82cafb8916c39799cbe5cb74
safeer <safeer.baig@shopdev.co>
2022-05-18 16:58:45 +0500

    fix: customer Services component

--- components/common/collection-card/descriptor.js
+++ components/common/collection-card/descriptor.js
@@ -1,36 +1,36 @@
-// const { Types } = require("@teamfabric/xpm");
+const { Types } = require("@teamfabric/xpm");
 
-// exports.default = Types.Component({
-//   id: "CollectionCard",
-//   label: "Explore our collection",
-//   description: ``,
+exports.default = Types.Component({
+  id: "CollectionCard",
+  label: "Explore our collection",
+  description: ``,
 
-//   attributes: {
-//     collectionCard: Types.Array({
-//       label: "collection card",
-//       children: Types.Shape({
-//         children: {
-//             collectionImage: Types.Image({
-//             label: "collection card image",
-//             url: Types.String({ label: "URL" }),
-//             altText: Types.String({ label: "Alt text" }),
-//           }),
-//           collectionImageKenaz: Types.Image({
-//             label: "collection card image",
-//             url: Types.String({ label: "URL" }),
-//             altText: Types.String({ label: "Alt text" }),
-//           }),
-//           collectionImageMissl: Types.Image({
-//             label: "collection card image",
-//             url: Types.String({ label: "URL" }),
-//             altText: Types.String({ label: "Alt text" }),
-//           }),
-//           collectionbutton: Types.String({ label: "Button Text" }),
-//           collectiontitle : Types.String({label : "Title"}),
-//           collectiontext : Types.String({label : "Text"}),
-//           collectionButtonLink : Types.String({ label : 'Button Link'}),
-//         },
-//       }),
-//     }),
-//   },
-// });
+  attributes: {
+    collectionCard: Types.Array({
+      label: "collection card",
+      children: Types.Shape({
+        children: {
+            collectionImage: Types.Image({
+            label: "collection card image",
+            url: Types.String({ label: "URL" }),
+            altText: Types.String({ label: "Alt text" }),
+          }),
+          collectionImageKenaz: Types.Image({
+            label: "collection card image",
+            url: Types.String({ label: "URL" }),
+            altText: Types.String({ label: "Alt text" }),
+          }),
+          collectionImageMissl: Types.Image({
+            label: "collection card image",
+            url: Types.String({ label: "URL" }),
+            altText: Types.String({ label: "Alt text" }),
+          }),
+          collectionbutton: Types.String({ label: "Button Text" }),
+          collectiontitle : Types.String({label : "Title"}),
+          collectiontext : Types.String({label : "Text"}),
+    //   collectionButtonLink : Types.String({ label : 'Button Link'}),
+        },
+      }),
+    }),
+  },
+});
