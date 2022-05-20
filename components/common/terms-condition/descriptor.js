fab6ab6ae02d90c1ee9e95f46e411b8da424f6e3
safeer <safeer.baig@shopdev.co>
2022-05-20 15:22:22 +0500

    fix: customer services component

---
+++ components/common/terms-condition/descriptor.js
@@ -0,0 +1,36 @@
+const { Types } = require("@teamfabric/xpm");
+
+exports.default = Types.Component({
+    id: "TermCondtion",
+    label: "TermCondtion",
+    description: ``,
+
+    attributes: {
+        hyperLinks: Types.Array({
+            label: 'HyperLink Array',
+            children: Types.Shape({
+                children: {
+                    name: Types.String({ label: 'Hyperlink Name' }),
+                    content: Types.String({ label: 'Hyperlink Content' }),
+                }
+            })
+        }),
+        sideBarBgcolor: Types.String({ label: 'Content Block Background Color' }),
+        contentBgcolor: Types.String({ label: 'Content Block Background Color' }),
+        icon: Types.Image({
+            label: 'Icon',
+            url: Types.String({ label: 'URL' }),
+            altText: Types.String({ label: 'Alt Text' }),
+        }),
+        accordion: Types.Array({
+            label: 'Accordion Array',
+            children: Types.Shape({
+                children: {
+                    heading: Types.String({ label: 'Question ' }),
+                    text: Types.RichText({ label: 'Answer' })
+                }
+            })
+        }),
+        title: Types.String({ label: 'Title' })
+    },
+});
