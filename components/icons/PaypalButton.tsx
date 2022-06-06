74058c9074c321f7a1b24406c3239427346945a4
haxceb <haseeb.shaukat@shopdev.co>
2022-06-06 10:24:51 +0500

    cart updated

--- components/icons/PaypalButton.tsx
+++ components/icons/PaypalButton.tsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 const PaypalButton = ({ ...props }) => {
-  return (
-    <Image src="/public/paypal-logo.png" alt="" width="100%" height="100%" />
-  );
+  return <Image src="/public/apple-logo.png" alt="" width="100%" height="100%" />;
 };
 
 export default PaypalButton;
