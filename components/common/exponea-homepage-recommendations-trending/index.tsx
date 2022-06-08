1779c1780a88c7056b25fd4cd2a4554bfe5d0693
yettimon <globalobjectiveart@gmail.com>
2022-06-08 10:13:32 +0200

    feat:exponea recommendations components for homepage

---
+++ components/common/exponea-homepage-recommendations-trending/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const ExponeaHomepageRecommendationsTrending: FC<{}> = () => {
+  return (
+    <>
+      <div id="exponea-homepage-recommendations-trending"></div>
+    </>
+  );
+};
+export default ExponeaHomepageRecommendationsTrending;
