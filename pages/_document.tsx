1f799cdd50f8b3f17598b336468a75765ef5143a
ahsanz1 <ahsanz639@gmail.com>
2022-06-09 12:59:19 +0500

    chore: font preload and lcp priority

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,13 +25,14 @@ class LazurdeDocument extends Document {
           ></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            crossOrigin="true"
           ></link>
-          <link
+          {/* <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin="anonymous"
-          ></link>
+          ></link> */}
           <link
             href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
