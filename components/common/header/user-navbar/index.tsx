f335cb7312718c8bfe84d72d6c332faf07af0bfd
Wasif-Hussain <wasif.hussain@shopdev.co>
2022-06-14 15:40:24 +0500

    build error fix

--- components/common/header/user-navbar/index.tsx
+++ components/common/header/user-navbar/index.tsx
@@ -36,7 +36,7 @@ const UserNavBar: FC<{ brandSideBar: BrandSidebarProps }> = ({
         "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNWRiMjliMGM0NjQ4MDM2YTI0NWZjMCIsInJvbGVzIjpbeyJpZCI6IjVlMTk2MjUwNWVmNjEyMDAwODlmM2IyMiJ9XSwicGVybWlzc2lvbnMiOltdLCJhY2NvdW50aWQiOiI2MjVkYjI5YWRlZTBlMjAwMDliMmRhNGQiLCJhY2NvdW50SWQiOm51bGwsInVzZXJUeXBlIjp7ImtpbmQiOiJSRUdJU1RFUkVEIn0sInRlbmFudElkIjoiNjFhNTEwZmEzN2JiNjQwMDA5YWNmNTVlIiwiaXNzdWVyIjoiNTczNzg1OTIzMjI0IiwiaWF0IjoxNjU0MTUzMzYxLCJleHAiOjE2NTQxNTUxNjF9.FLBjzjjR3g1zreH03aIE9B92H5y1HL6RfhwoePFbKeASfqq2RcyGqkKiexRTELDTPMOJEa9XXklsqfaegYS-fKrEXoIjjHv4KpolommWzaSINL5C__zljx7QZtF5sRtyYKPPlwEcuPtdMJTCERIfyDIHsMF4oehEVvN-cd6DwOA";
 
       const wishlistArray = await getWishList(authToken);
-      setAllWishListProducts(wishlistArray?.data?.items);
+      setAllWishListProducts && setAllWishListProducts(wishlistArray?.data?.items);
       typeof window !== "undefined" &&
         window.sessionStorage.setItem(
           "wishListArray",
