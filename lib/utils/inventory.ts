791a2a1c8fa06d998c60b1611f1f57f01960395e
Wasif-Hussain <wasif.hussain@shopdev.co>
2022-06-16 19:11:37 +0500

    cart stock limit and error message added

--- lib/utils/inventory.ts
+++ lib/utils/inventory.ts
@@ -2,13 +2,37 @@ import Axios from "axios";
 import ENDPOINTS from "lib/api/endpoints";
 import HEADERS from "lib/api/headers";
 import { ErrorObject } from "lib/types/common";
-import { OMS_DOMAIN } from "general-config";
+import { OMS_DOMAIN, WISHLIST } from "general-config";
 
-export const getInventoryByIds = async (authToken: any) => {
+export const getInventoryByIds = async (authToken: string, itemId: number) => {
   try {
-    const response = await Axios.get(`${OMS_DOMAIN}${ENDPOINTS.INVENTORY.GET_BY_IDS}`, {
-      headers: { ...HEADERS.common, Authorization: authToken },
-    });
+    const response = await Axios.get(
+      `${OMS_DOMAIN}${ENDPOINTS.INVENTORY.GET_BY_IDS(itemId)}`,
+      {
+        headers: { ...HEADERS.common, Authorization: authToken },
+      }
+    );
+    return response;
+  } catch (error: unknown) {
+    console.log(
+      "Error while fecthing reviews",
+      (error as ErrorObject)?.message
+    );
+  }
+};
+
+export const getInventoryAuth = async () => {
+  try {
+    const response = await Axios.post(
+      `${WISHLIST}${ENDPOINTS.INVENTORY.GET_AUTH}`,
+      {
+        username: "test@fabric.inc",
+        password: "test1234",
+      },
+      {
+        headers: { ...HEADERS.common },
+      }
+    );
     return response;
   } catch (error: unknown) {
     console.log(
