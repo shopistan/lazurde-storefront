21524135a4c6ebc31fa8e8436710c431dd55a7f3
ahsanz1 <ahsanz639@gmail.com>
2022-06-16 14:02:07 +0500

    chore: gstatic and googleapis links commented

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,12 +18,12 @@ class LazurdeDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin="anonymous"
-          ></link>
+          ></link> */}
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="preload"
