9e896caba6cc22f022ed815cf3bbfa5046d32ced
ahsanz1 <ahsanz639@gmail.com>
2022-06-16 13:34:56 +0500

    chore: CO added back

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,21 +19,21 @@ class LazurdeDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          {/* <link
+          <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin="anonymous"
-          ></link> */}
+          ></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="preload"
             as="style"
-            // crossOrigin='anonymous'
+            crossOrigin="anonymous"
           ></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
-            // crossOrigin='anonymous'
+            crossOrigin="anonymous"
           ></link>
           {/* <link
             rel="preconnect"
