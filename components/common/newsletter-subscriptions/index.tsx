2e9d00b3ef2d651c3bfea7f6a7b0d9b6dc51345e
Arsal Waseem <arsal.waseem@shopdev.co>
2022-07-06 13:50:07 +0500

    feat: newsletter page updated

--- components/common/newsletter-subscriptions/index.tsx
+++ components/common/newsletter-subscriptions/index.tsx
@@ -1,18 +1,22 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
 import { AppContext } from "lib/context/index";
 import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
 
 const NewsSubscriptions = (): JSX.Element => {
-
   const { t } = useTranslation("common");
   const { appState } = useContext(AppContext);
   const [renderCom, setRenderCom] = useState(false);
+  const router = useRouter();
   useEffect(() => {
     setRenderCom(true);
   }, []);
 
+  const handleAction = () => {
+    router.push("/newsletter");
+  };
   return (
     <>
       <div className={styles["account-newsletter-container"]}>
@@ -27,7 +31,9 @@ const NewsSubscriptions = (): JSX.Element => {
             />
           </div>
           <div className={styles["account-newsletter-heading"]}>
-          {appState?.lang == "en" ? "Newsletter Subscriptions" : t("Newsletter Subscriptions")}
+            {appState?.lang == "en"
+              ? "Newsletter Subscriptions"
+              : t("Newsletter Subscriptions")}
           </div>
         </div>
         <div className={styles["account-newsletter-content-section"]}>
@@ -40,7 +46,11 @@ const NewsSubscriptions = (): JSX.Element => {
                   name="All-Subscriptions"
                   value="All-Subscriptions"
                 />
-                <label>{appState?.lang == "en" ? "All Subscriptions" : t("All Subscriptions")}</label>
+                <label>
+                  {appState?.lang == "en"
+                    ? "All Subscriptions"
+                    : t("All Subscriptions")}
+                </label>
                 <br />
               </div>
               <div className={styles["Checkbox-2"]}>
@@ -50,7 +60,12 @@ const NewsSubscriptions = (): JSX.Element => {
                   name="Lazurde-Subscriptions"
                   value="Lazurde-Subscriptions"
                 />
-                <label> {appState?.lang == "en" ? "L’azurde Subscriptions" : t("L’azurde Subscriptions")}</label>
+                <label>
+                  {" "}
+                  {appState?.lang == "en"
+                    ? "L’azurde Subscriptions"
+                    : t("L’azurde Subscriptions")}
+                </label>
                 <br />
               </div>
               <div className={styles["Checkbox-3"]}>
@@ -60,7 +75,11 @@ const NewsSubscriptions = (): JSX.Element => {
                   name="MissL-Subscriptions"
                   value="MissL-Subscriptions"
                 />
-                <label>{appState?.lang == "en" ? "Miss L ‘ Subscriptions" : t("Miss L ‘ Subscriptions")}</label>
+                <label>
+                  {appState?.lang == "en"
+                    ? "Miss L ‘ Subscriptions"
+                    : t("Miss L ‘ Subscriptions")}
+                </label>
                 <br />
               </div>
               <div className={styles["Checkbox-4"]}>
@@ -70,7 +89,11 @@ const NewsSubscriptions = (): JSX.Element => {
                   name="Kenaz-Subscriptions"
                   value="Kenaz-Subscriptions"
                 />
-                <label>{appState?.lang == "en" ? "Kenaz Subscriptions" : t("Kenaz Subscriptions")}</label>
+                <label>
+                  {appState?.lang == "en"
+                    ? "Kenaz Subscriptions"
+                    : t("Kenaz Subscriptions")}
+                </label>
                 <br />
               </div>
               <div className={styles["Checkbox-5"]}>
@@ -80,12 +103,21 @@ const NewsSubscriptions = (): JSX.Element => {
                   name="InStyle-Subscriptions"
                   value="InStyle-Subscriptions"
                 />
-                <label>{appState?.lang == "en" ? "InStyle Subscriptions" : t("InStyle Subscriptions")}</label>
+                <label>
+                  {appState?.lang == "en"
+                    ? "InStyle Subscriptions"
+                    : t("InStyle Subscriptions")}
+                </label>
                 <br />
               </div>
             </div>
-            <div className={styles["Save-button"]}>
-              <input type="submit" value={appState?.lang == "en" ? "Save" : t("save")} />
+            <div
+              className={styles["Save-button"]}
+              onClick={() => {
+                handleAction();
+              }}
+            >
+              {appState?.lang == "en" ? "Save" : t("save")}
             </div>
           </form>
         </div>
