8acfe8e27f8c2d9d4532eeef9dd0747ad6740c79
Arsal Waseem <arsal.waseem@shopdev.co>
2022-07-06 21:06:17 +0500

    fix: descriptor updated

--- components/common/newsletter-signup/descriptor.js
+++ components/common/newsletter-signup/descriptor.js
@@ -6,22 +6,15 @@ exports.default = Types.Component({
   description: ``,
 
   attributes: {
-    newsletterArray: Types.Array({
-      label: "Newsletter Banner",
-      children: Types.Shape({
-        children: {
-          backgroundImage: Types.Image({
-            label: "Background Image",
-            url: Types.String({ label: "URL" }),
-            altText: Types.String({ label: "Alt text" }),
-          }),
-          bannerText: Types.String({ label: "Banner Heading" }),
-          bannerBodyText: Types.String({ label: "Banner Text" }),
-          heading: Types.String({ label: "Newsletter Heading" }),
-          upperText: Types.String({ label: "Newsletter Description" }),
-          lowerText: Types.String({ label: "Newsletter Text" }),
-        },
-      }),
+    backgroundImage: Types.Image({
+      label: "Background Image",
+      url: Types.String({ label: "URL" }),
+      altText: Types.String({ label: "Alt text" }),
     }),
+    bannerText: Types.String({ label: "Banner Heading" }),
+    bannerBodyText: Types.String({ label: "Banner Text" }),
+    heading: Types.String({ label: "Newsletter Heading" }),
+    upperText: Types.String({ label: "Newsletter Description" }),
+    lowerText: Types.String({ label: "Newsletter Text" }),
   },
 });
