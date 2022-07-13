11ea3900124bf615c981f156af6ba27c6155aaa8
UmairKhan <umair.baloch@shopdev.co>
2022-07-13 17:35:58 +0500

    chore: unit testing for account page

--- components/common/account-information/account-overview/index.tsx
+++ components/common/account-information/account-overview/index.tsx
@@ -1,25 +1,32 @@
 import React, { useContext } from "react";
 import Image from "next/image";
-import styles from "./account-section.module.scss";
+import styles from "./style.module.scss";
 import { AppContext } from "lib/context/index";
 import useTranslation from "next-translate/useTranslation";
 
-const AccountSection = () => {
+const AccountOverView = () => {
   const { appState } = useContext(AppContext);
   const { t } = useTranslation("common");
   return (
     <>
       <div className={styles["account-detail-section"]}>
         <div className={styles["account-image-section"]}>
-          <div className={styles["account-image-text"]}>
+          <div data-testid="heading" className={styles["account-image-text"]}>
             {appState?.lang == "en" ? `Welcome to your account` : t("welcome")}
           </div>
           <div className={styles["account-right"]}>
-            <Image src={"/main-image.png"} width={650} height={760} layout='responsive' />
+            <Image
+              role="image"
+              src={"/main-image.png"}
+              width={650}
+              height={760}
+              layout="responsive"
+              alt="main-image"
+            />
           </div>
         </div>
       </div>
     </>
   );
 };
-export default AccountSection;
+export default AccountOverView;
