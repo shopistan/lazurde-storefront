57379337013402f152aa34548810ff1993821c6f
Arsal Waseem <arsal.waseem@shopdev.co>
2022-07-15 19:37:46 +0500

    fix: update descriptor data

--- components/common/corporate-governance-and-investor-relations/descriptor.js
+++ components/common/corporate-governance-and-investor-relations/descriptor.js
@@ -27,16 +27,20 @@ exports.default = Types.Component({
             }),
           }),
           dividend: Types.Array({
-            label: "Dividend History",
+            label: "Dividend",
             children: Types.Shape({
               children: {
                 heading: Types.String({ label: "Heading" }),
                 text: Types.RichText({ label: "Paragraph" }),
                 dividendHistory: Types.Array({
                   label: "Dividend Table",
-                  heading: Types.String({ label: "Heading" }),
-                  year: Types.String({ label: "Year" }),
-                  value: Types.String({ label: "Value" }),
+                  children: Types.Shape({
+                    children: {
+                      heading: Types.String({ label: "Heading" }),
+                      year: Types.String({ label: "Year" }),
+                      value: Types.String({ label: "Value" }),
+                    },
+                  }),
                 }),
                 content: Types.RichText({ label: "Content" }),
               },
